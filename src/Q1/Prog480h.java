b75a8a904a268cef7ee1545972e510f2968d3e25
guernsey.l <guernsey.l@stu.janesville.k12.wi.us>
2025-09-19 12:17:17 -0500

    Commit 9/19/2025

--- src/Q1/Prog480h.java
+++ src/Q1/Prog480h.java
@@ -7,15 +7,19 @@ public class Prog480h {
     public static void main(String[] args) throws IOException {
         Scanner File = new Scanner(new File("Langdat/prog480h.dat"));
         int[][] Key = new int[12][24];
+        for     (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 24; j++) {
+                Key[i][j] = -1;
+            }
+        }
         System.out.print("ID     ");
         for (int i = 1; i < 11; ++i) {
             Key[i][0] = i;
             System.out.print(Key[i][0] + " ");
         }
-        System.out.print("Right  Grade");
-        System.out.println();
-        System.out.print("       ");
-        while (File.hasNextLine()) {
+        System.out.println("Correct:  Grade:");
+        System.out.print("Key:   ");
+        while  (File.hasNextLine()) {
             if (File.nextInt() == -1) {
                 for (int i = 1; i < 11; ++i) {
                     Key[i][1] = File.nextInt();
@@ -23,15 +27,40 @@ public class Prog480h {
                 }
             }
             System.out.println();
-            for (int r = 2; r < 24; ++r) {
-                for (int c = 0; c < 11; ++c) {
+            for        (int r = 2; r < Key[0].length-2; ++r) {
+                for    (int c = 0; c < Key.length-1; ++c) {
                     if (!File.hasNext()) { break; }
                     Key[c][r] = File.nextInt();
                     System.out.print(Key[c][r] + " ");
                     if (c == 0) { System.out.print("   "); }
                 }
-                System.out.println();
+                if (Key[0][r] >= 100) {
+                    Key[Key.length - 1][r] = 0;
+                    int correct = 0;
+                    for (int c = 1; c <= Key.length - 1; ++c) {
+                        if (Key[c][r] == Key[c][1] && Key[c][r] > -1) {
+                            ++correct;
+                            Key[Key.length - 1][r] = correct;
+                        }
+                    }
+                    if (Key[Key.length - 1][r] > -1) {
+                        System.out.print("   " + Key[Key.length - 1][r] + "         ");
+                        if (Key[Key.length - 1][r] >= 9) {
+                            System.out.print("A");
+                        } else if (Key[Key.length - 1][r] >= 7) {
+                            System.out.print("C");
+                        } else if (Key[Key.length - 1][r] < 7) {
+                            System.out.print("F");
+                        }
+                    }
+                    System.out.println();
+                }
             }
+            System.out.print("Correct: ");
+            for (int i = 1; i < 11; ++i) {
+                System.out.print(Key[i][2] + " ");
+            }
+
         }
     }
 }
--- src/Q1/Prog492h.java
+++ src/Q1/Prog492h.java
@@ -24,7 +24,8 @@ public class Prog492h {
             }
             System.out.println();
         }
-        System.out.println("\n\n");
+        for (int i = 0; i < 15; ++i) {
+            System.out.println("\n\n");
             String[][] ansBoard = new String[30][30];
             for (String[] row : board) {
                 for (String index : row) {
@@ -32,82 +33,78 @@ public class Prog492h {
                 }
                 System.out.println();
 
-        for (r = 0; r < board.length; ++r) {
-            for (int c = 0; c < board[r].length; ++c) {
-                int count = 0;
-                //1
-                if (r >= 1) {
-                    String ch = board[r - 1][c];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //2
-                if ((r >= 1) && (c <= board[0].length - 2)) {
-                    String ch = board[r - 1][c + 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //3
-                if (c <= board[0].length - 2) {
-                    String ch = board[r][c + 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //4
-                if (r <= board.length - 2 && c <= board[0].length - 2) {
-                    String ch = board[r + 1][c + 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //5
-                if (r <= board.length - 2) {
-                    String ch = board[r + 1][c];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //6
-                if (r <= board.length - 2 && c >= 1) {
-                    String ch = board[r + 1][c - 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //7
-                if (c >= 1) {
-                    String ch = board[r][c - 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
-                //8
-                if (r >= 1 && c >= 1) {
-                    String ch = board[r - 1][c - 1];
-                    if (ch.equals("*")) {
-                        ++count;
-                    }
-                }
+                for (r = 0; r < board.length; ++r) {
+                    for (int c = 0; c < board[r].length; ++c) {
+                        int count = 0;
+                        //1
+                        if (r >= 1) {
+                            String ch = board[r - 1][c];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //2
+                        if ((r >= 1) && (c <= board[0].length - 2)) {
+                            String ch = board[r - 1][c + 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //3
+                        if (c <= board[0].length - 2) {
+                            String ch = board[r][c + 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //4
+                        if (r <= board.length - 2 && c <= board[0].length - 2) {
+                            String ch = board[r + 1][c + 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //5
+                        if (r <= board.length - 2) {
+                            String ch = board[r + 1][c];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //6
+                        if (r <= board.length - 2 && c >= 1) {
+                            String ch = board[r + 1][c - 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //7
+                        if (c >= 1) {
+                            String ch = board[r][c - 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
+                        //8
+                        if (r >= 1 && c >= 1) {
+                            String ch = board[r - 1][c - 1];
+                            if (ch.equals("*")) {
+                                ++count;
+                            }
+                        }
 
-                if (count <= 1 || count >= 4) {
-                    ansBoard[r][c] = ".";
+                        if (count <= 1 || count >= 4) {
+                            ansBoard[r][c] = ".";
+                        }
+                        if (count == 3) {
+                            ansBoard[r][c] = "*";
+                        } else {
+                            ansBoard[r][c] = board[r][c];
+                        }
+                    }
                 }
-                if (count == 3) {
-                    ansBoard[r][c] = "*";
-                } else {
-                    ansBoard[r][c] = board[r][c];
-                }
+                board = ansBoard;
             }
         }
-        board = ansBoard;
-                for (String[] row : board) {
-                    for (String index : row) {
-                        System.out.print(index + " ");
-                    }
-                    System.out.println();
-        System.out.println("\n\n");
     }
 }
