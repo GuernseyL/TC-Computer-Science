fcc549a405a7e909e6f5130e5584101d1464a604
guernsey.l <guernsey.l@stu.janesville.k12.wi.us>
2025-10-30 12:19:33 -0500

    Commit 10/30/2025

--- src/Q1/Java/Langdocs/Prog285bCircular/Prog285bCircularHelper.java
+++ src/Q1/Java/Langdocs/Prog285bCircular/Prog285bCircularHelper.java
@@ -1,17 +1,17 @@
 package Q1.Java.Langdocs.Prog285bCircular;
 
-public class Lang285bDoubleHelper {
+public class Prog285bCircularHelper {
     private Node myroot;
 
-    public Lang285bDoubleHelper() {
+    public Prog285bCircularHelper() {
         myroot = null;
     }
 
     public void addfront(Node node) {
         if (myroot == null) {
             myroot = node;
-            node.myNext = myroot;
-            node.myPrev = myroot;
+            myroot.myNext = myroot;
+            myroot.myPrev = myroot;
         }
         else {
             node.myNext = myroot;
@@ -21,7 +21,7 @@ public class Lang285bDoubleHelper {
                 temp = temp.myNext;
             }
             temp.myNext = node;
-            temp.myNext.myPrev = node;
+            temp.myNext.myPrev = temp;
         }
     }
 
@@ -45,17 +45,48 @@ public class Lang285bDoubleHelper {
     }
 
     public void remove0() {
-        if (myroot != null && myroot.getCom() == 0.0) {
+        if (myroot.getCom() == 0.0) {
+            myroot.myNext.myPrev = myroot.myPrev;
+            myroot.myPrev.myNext = myroot.myNext;
             myroot = myroot.myNext;
         }
+        Node temp = myroot.myNext;
 
-        Node temp = myroot;
-
-        while(temp != null) {
+        while (temp != myroot) {
             if (temp.getCom() == 0.0) {
+                temp.myNext.myPrev = temp.myPrev;
                 temp.myPrev.myNext = temp.myNext;
             }
             temp = temp.myNext;
         }
     }
 }
+/*
+101	17	2250.0	213.75
+103	5	4000.0	3000.0
+117	3	7350.0	0.0
+118	8	7350.0	574.75
+125	5	6500.0	502.5
+138	17	6375.0	605.625
+192	8	8125.0	640.625
+203	8	3250.0	2437.5
+218	5	5000.0	3750.0
+235	5	5250.0	396.25
+264	17	4150.0	394.25
+291	17	750.0	71.25
+
+
+
+101	17	2250.0	213.75
+103	5	4000.0	3000.0
+118	8	7350.0	574.75
+125	5	6500.0	502.5
+138	17	6375.0	605.625
+192	8	8125.0	640.625
+203	8	3250.0	2437.5
+218	5	5000.0	3750.0
+235	5	5250.0	396.25
+264	17	4150.0	394.25
+291	17	750.0	71.25
+ */
+
--- src/Q1/Java/Langdocs/Prog285bCircular/Prog285bCircularLinkedList.java
+++ src/Q1/Java/Langdocs/Prog285bCircular/Prog285bCircularLinkedList.java
@@ -1,4 +1,32 @@
 package Q1.Java.Langdocs.Prog285bCircular;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Prog285bCircularLinkedList {
+    public static void main(String[] args) throws IOException {
+        Scanner file = new Scanner(new File("Langdat/prog285b.txt"));
+
+        Prog285bCircularHelper helper = new Prog285bCircularHelper();
+
+        while(file.hasNext()){
+            int id = file.nextInt();
+            int c = file.nextInt();
+            double s = file.nextDouble();
+            Node markiplier = new Node(id, c, s);
+            helper.addfront(markiplier);
+        }
+
+        helper.calcall();
+
+        helper.print();
+
+        System.out.println("\n\n");
+
+        helper.remove0();
+
+        helper.print();
+    }
 }
