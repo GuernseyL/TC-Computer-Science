03aa8645b894a6a58936b7d2cd31ff12e9192ef7
guernsey.l <guernsey.l@stu.janesville.k12.wi.us>
2025-11-12 10:31:45 -0600

    Commit 11/12/2025

--- src/Q1/Java/BluePelican/StudentClassification/StudentClassification.java
+++ src/Q1/Java/BluePelican/StudentClassification/StudentClassification.java
@@ -7,38 +7,29 @@ import java.util.*;
 public class StudentClassification {
     public static void main(String[] args) throws IOException {
         Scanner file = new Scanner(new File("src/Q1/Java/BluePelican/StudentClassification/StudentClassification.txt"));
-        Map<String, Set> map = new TreeMap<>();
-        Set<String> SetFreshmen = new HashSet<>();
-        Set<String> SetSophmore = new HashSet<>();
-        Set<String> SetJunior = new HashSet<>();
-        Set<String> SetSenior = new HashSet<>();
+        Map<String, TreeSet<String>> map = new TreeMap<>();
+        TreeSet<String>[] set = new TreeSet[4];
+
 
         while (file.hasNextLine()) {
             String[] line = file.nextLine().split(" ");
-            if (line[0].equals("Freshman")) {
-                SetFreshmen.add(line[1]);
-            }
-            if (line[0].equals("Sopherman")) {
-                SetSophmore.add(line[1]);
-            }
-            if (line[0].equals("Junior")) {
-                SetJunior.add(line[1]);
-            }
-            if (line[0].equals("Senior")) {
-                SetSenior.add(line[1]);
-            }
+            set[(line[0].equals("Freshman")) ? 0 : (line[0].equals("Sophomore")) ? 1 : (line[0].equals("Junior")) ? 2 : 3].add(line[1]);
         }
-        map.put("Freshman", SetFreshmen);
-        map.put("Sophmore", SetSophmore);
-        map.put("Junior", SetJunior);
-        map.put("Senior", SetSenior);
 
-        for (Map.Entry<String, Set> entry : map.entrySet()) {
+        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
             String key = entry.getKey();
             Set value = entry.getValue();
+            System.out.print(key + ": ( ");
             for (Object o : value) {
-
+                System.out.print(o + " ");
             }
+            System.out.println(")");
         }
     }
 }
+/*
+Freshman: ( Fred )
+Junior: ( Betty David Ruth )
+Senior: ( Agnes Bill Boaz Jezebel )
+Sophomore: ( Ahab Julie Solomon )
+ */
