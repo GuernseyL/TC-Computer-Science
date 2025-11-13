45b3958488c03e7fa6430199f7186a765470288f
guernsey.l <guernsey.l@stu.janesville.k12.wi.us>
2025-11-13 10:28:54 -0600

    Commit 11/13/2025

--- src/Q1/Java/BluePelican/StudentClassification/StudentClassification.java
+++ src/Q1/Java/BluePelican/StudentClassification/StudentClassification.java
@@ -2,34 +2,34 @@ package Q1.Java.BluePelican.StudentClassification;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Key;
 import java.util.*;
 
 public class StudentClassification {
     public static void main(String[] args) throws IOException {
         Scanner file = new Scanner(new File("src/Q1/Java/BluePelican/StudentClassification/StudentClassification.txt"));
-        Map<String, TreeSet<String>> map = new TreeMap<>();
-        TreeSet<String>[] set = new TreeSet[4];
+        Map<String, Set<String>> map = new HashMap<>();
+        String[] keys = {"Freshman", "Sophomore", "Junior", "Senior"};
+        for (int lcv = 0; lcv < keys.length; ++lcv) {
+            map.put(keys[lcv], new TreeSet<>());
+        }
 
 
         while (file.hasNextLine()) {
-            String[] line = file.nextLine().split(" ");
-            set[(line[0].equals("Freshman")) ? 0 : (line[0].equals("Sophomore")) ? 1 : (line[0].equals("Junior")) ? 2 : 3].add(line[1]);
+            map.get(file.next()).add(file.next());
         }
 
-        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Set value = entry.getValue();
-            System.out.print(key + ": ( ");
-            for (Object o : value) {
-                System.out.print(o + " ");
-            }
-            System.out.println(")");
+        for (String key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key) + "\n");
         }
     }
 }
 /*
-Freshman: ( Fred )
-Junior: ( Betty David Ruth )
-Senior: ( Agnes Bill Boaz Jezebel )
-Sophomore: ( Ahab Julie Solomon )
+Sophomore:[Ahab, Julie, Solomon]
+
+Junior:[Betty, David, Ruth]
+
+Senior:[Agnes, Bill, Boaz, Jezebel]
+
+Freshman:[Fred]
  */
