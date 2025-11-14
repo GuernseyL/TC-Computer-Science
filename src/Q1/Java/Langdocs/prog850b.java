c75e6563a41e8c9ea9d4dd66fb379543abcd340d
guernsey.l <guernsey.l@stu.janesville.k12.wi.us>
2025-11-14 10:31:32 -0600

    Commit 11/14/2025

--- src/Q1/Java/Langdocs/prog850b.java
+++ src/Q1/Java/Langdocs/prog850b.java
@@ -8,15 +8,13 @@ import java.util.TreeMap;
 
 public class prog850b {
     public static void main(String[] args) throws IOException {
-        Scanner file = new Scanner(new File("langdat/prog850b.txt"));
-        Map<String, Integer[]> map = new TreeMap<>();
+        Scanner file = new Scanner(new File("langdat/prog850b.csv"));
+        Map<String, Integer> map = new TreeMap<>();
         while (file.hasNextLine()) {
-            String name = file.next();
-            Integer[] data = {file.nextInt(), 0};
-            file.next();
-            file.next();
-            file.next();
-            map.put(name, map.getOrDefault(name, 0) + data);
+            String[] line = file.nextLine().split(",");
+            if (!line[1].equals("total")) {
+                map.put(line[0], map.getOrDefault(line[0], 0) + Integer.parseInt(line[line.length - 1]));
+            }
         }
         //map.put(line[0], Integer.parseInt(line[1]));
         for (String key : map.keySet()) System.out.printf(key + "\t" + map.get(key));
